215f0842f22c7f62f2636b8a6e36639407c74818
Grant Folgate <gfolgate@outlook.com>
2023-04-15 22:09:31 -0400

    add in config file and db connection logic

--- run.py
+++ run.py
@@ -1,7 +1,74 @@
-
 import os
+import sys
+import json
+
 from app import application
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+def parse_app_config(app, config_file):
+    """
+    Load and parse the input JSON file for app specific data
+
+    Args:
+        config_file (str): The name of the JSON file to parse.
+
+    Returns:
+        A dictionary containing the parsed app config data.
+
+    """
+    # Load the JSON file into a dictionary
+    with open(config_file) as f:
+        cfg_data = json.load(f)
+
+    return cfg_data
+    
+def bind_cfg_deps(app, cfg_data):
+    """
+    Add config specific attributes to the application class
+    for other services to reference within the project
+
+    Args:
+        app (obj): The encompassing flask application object
+        cfg_data (dict): A dictionary containing the parsed app
+        config data.
+
+    Returns:
+        None
+
+    """
+    if "host_info" in cfg_data:
+        if "host" in cfg_data["host_info"]:
+            setattr(app, "host_name", cfg_data["host_info"]["host"])
+        if "port" in cfg_data["host_info"]:
+            setattr(app, "host_port", cfg_data["host_info"]["port"])
+
+    if "db_info" in cfg_data:
+        if "db_name" in cfg_data["db_info"]:
+            setattr(app, "db_name", cfg_data["db_info"]["db_name"])
+        if "conn_str" in cfg_data["db_info"]:
+            setattr(app, "db_conn_str", cfg_data["db_info"]["conn_str"])
+            
+            # Create a new client and connect to the server
+            client = MongoClient(app.db_conn_str, server_api=ServerApi('1'))
+            setattr(app, "mongo_client", client)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4433))
-    application.run('127.0.0.1', port=port)
+
+    if len(sys.argv) != 2:
+        print('Please enter an input configuration file')
+        sys.exit()
+    
+    config_file = sys.argv[1]
+
+    cfg_data = parse_app_config(application, config_file)
+    bind_cfg_deps(application, cfg_data)
+    
+    host_name = getattr(application, 'host_name')
+    host_port = getattr(application, 'host_port')
+
+    if host_name and host_port:
+        port = int(os.environ.get("PORT", host_port))
+        application.run(host_name, port=port)
+    else:
+        print('Please specify a host port and host name in the cfg file')
